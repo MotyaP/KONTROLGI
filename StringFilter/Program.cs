fed14737d03d653733da0f4edfa9a4693566f377
MotyaP <matvey092003@gmail.com>
2025-06-08 23:54:32 +0300

    Add files via upload

---
+++ StringFilter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] inputArray = { "Hello", "2", "world", ":-)" };
+        
+        int count = 0;
+        foreach (string s in inputArray)
+        {
+            if (s.Length <= 3) count++;
+        }
+        
+        string[] resultArray = new string[count];
+        int index = 0;
+        foreach (string s in inputArray)
+        {
+            if (s.Length <= 3)
+            {
+                resultArray[index] = s;
+                index++;
+            }
+        }
+        
+        Console.WriteLine("Результат: [" + string.Join(", ", resultArray) + "]");
+    }
+}
